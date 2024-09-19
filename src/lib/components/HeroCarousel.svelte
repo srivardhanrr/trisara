b7df6d01eba78fc637efb1fb1b20047909a194cf
Srivardhan <srivardhan.singh.rathore@gmail.com>
2024-09-20 01:58:22 +0530

    changes

--- src/lib/components/HeroCarousel.svelte
+++ src/lib/components/HeroCarousel.svelte
@@ -3,12 +3,24 @@
 
 	import Autoplay from 'embla-carousel-autoplay';
 
-	export let heroImages;
+	export let heroImages = [
+		{
+			title: 'Hero 1',
+			image: '/images/home/hero1.jpg'
+		},
+		{
+			title: 'Hero 1',
+			image: '/images/home/hero3.jpg'
+		},
+		{
+			title: 'Hero 1',
+			image: '/images/home/hero2.jpg'
+		}
+	];
 
 	const plugin = Autoplay({ delay: 4000, stopOnInteraction: true });
 </script>
 
-<!-- {#if heroImages.length > 0}
 <Carousel.Root
 	opts={{ align: 'start' }}
 	plugins={[plugin]}
@@ -19,15 +31,20 @@
 	<Carousel.Content>
 		{#each heroImages as heroImage}
 			<Carousel.Item>
-				<div class="relative md:min-h-[calc(100vh-80px)] h-64 overflow-hidden">
+				<div class="relative h-64 overflow-hidden md:min-h-[calc(100vh-80px)]">
 					<img
-						class="left-0 top-0 h-full w-full object-fit"
+						class="object-fit left-0 top-0 h-full w-full"
 						src={heroImage.image}
-						alt="{heroImage.title}"
+						alt={heroImage.title}
 					/>
+					<!-- <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
+						<h2 class="text-xl text-white">{item.name}</h2>
+					</div> -->
 				</div>
 			</Carousel.Item>
 		{/each}
-	</Carousel.Content>	
+		<!--    </div>-->
+	</Carousel.Content>
+	<!-- <Carousel.Previous class="ml-16" />
+	<Carousel.Next class="mr-16" /> -->
 </Carousel.Root>
-{/if} -->
